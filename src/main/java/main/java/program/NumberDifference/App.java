1e11e34bf094a61fe943e43512d4fb6849ba3ab2
nabilputrah <nabilputra28.ns@gmail.com>
2022-04-16 15:21:41 +0700

    update

---
+++ src/main/java/main/java/program/NumberDifference/App.java
@@ -0,0 +1,13 @@
+package main.java.program.NumberDifference;
+
+/**
+ *
+ * @author nabil
+ */
+
+public class App {
+    public static void main(String[] args) {
+    	NumberDifferenceFlow application = new NumberDifferenceFlow();
+    	application.execute(); // call application process
+    }
+}
